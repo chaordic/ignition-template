74eae5e1b66b78b3ef403b87a211768d894fc708
Allan Douglas R. de Oliveira <allandouglas@gmail.com>
2015-05-10 16:27:39 -0300

    Updated core and hadoop/spark

--- project/build.scala
+++ project/build.scala
@@ -15,9 +15,10 @@ object IgnitionBuild extends Build {
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings"),
       // Because we can't run two spark contexts on same VM
       parallelExecution in Test := false,
-      libraryDependencies += ("org.apache.spark" %% "spark-core" % "1.2.1" % "provided")
+      resolvers += "Cloudera Repository" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
+      libraryDependencies += ("org.apache.spark" %% "spark-core" % "1.3.0" % "provided")
         .exclude("org.apache.hadoop", "hadoop-client"),
-      libraryDependencies += ("org.apache.hadoop" % "hadoop-client" % "1.0.4" % "provided"),
+      libraryDependencies += ("org.apache.hadoop" % "hadoop-client" % "2.0.0-cdh4.7.1" % "provided"),
       libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.0.6",
       libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
   )
