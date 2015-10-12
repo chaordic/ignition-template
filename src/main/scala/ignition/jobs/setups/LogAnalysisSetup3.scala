f04cb08acb152358e3f31eaef4371ee85d7980d2
Allan Douglas R. de Oliveira <allandouglas@gmail.com>
2015-10-12 18:57:50 -0300

    Added example

---
+++ src/main/scala/ignition/jobs/setups/LogAnalysisSetup3.scala
@@ -0,0 +1,46 @@
+package ignition.jobs.setups
+
+import ignition.core.jobs.CoreJobRunner.RunnerContext
+
+object LogAnalysisSetup3 {
+  
+  val statusLines: Seq[String] =
+     """1 /comprar 200
+       |2 /comprar 200
+       |3 /comprar 200
+       |4 /listar 200
+       |5 /listar 200
+       |6 /ver 200
+       |7 /ver 200
+       |8 /excluir 200
+       |9 /excluir 200
+       |10 /teste 501
+       |11 /teste 200
+       |12 /listrr 404""".stripMargin.split("\n")
+
+   def run(runnerContext: RunnerContext) {
+
+     val sc = runnerContext.sparkContext
+     val sparkConfig = runnerContext.config
+
+     val statusRDD = sc.parallelize(statusLines)
+
+     val pages = statusRDD map { x =>
+       x.split(" ")(1)
+     }
+
+     val total = statusRDD.count()
+
+     pages map {
+       (_, 1)
+     } reduceByKey {
+       _ + _
+     } sortBy({case (page, count) => count}, false) take(5) foreach { case (page, count) =>
+       println(s"${page} ${count} ${count.toFloat / total}")
+     }
+
+
+
+   }
+
+ }
